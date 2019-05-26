a721f6d29caf8ff311b322b3c0175dee07f00494
Bolla <Srinivas_Bolla@Dellteam.com>
2019-05-26 23:06:37 +0530

    first commit

---
+++ sql/sql-scripts.sql
@@ -0,0 +1,11 @@
+CREATE TABLE vasu.articles (
+  `article_id` int(5) NOT NULL AUTO_INCREMENT,
+  `title` varchar(200) NOT NULL,
+  `category` varchar(100) NOT NULL,
+  PRIMARY KEY (`article_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+
+INSERT INTO vasu.articles (`article_id`, `title`, `category`) VALUES
+	(1, 'Java Concurrency', 'Java'),
+	(2, 'Hibernate HQL ', 'Hibernate'),
+	(3, 'Spring MVC with Hibernate', 'Spring');
